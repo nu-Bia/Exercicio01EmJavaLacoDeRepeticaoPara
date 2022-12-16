41a4dc3fd49d4ac22a3469ef999b1fefcec793fd
Núbia <112019351+nu-Bia@users.noreply.github.com>
2022-12-16 05:16:38 -0300

    Add files via upload

---
+++ ExercicioA.java
@@ -0,0 +1,22 @@
+//a)   Apresentar os quadrados dos números inteiros de 15 a 200. 
+package Exercicios;
+
+import java.util.Scanner;
+
+public class ExercicioA {
+	
+public static void main (String[]args) {
+
+int i, quadrado;
+
+for(i=15;i<=200;i++) {
+	
+quadrado = i*i; 
+
+System.out.println("O quadrado de " + i + " é " + quadrado); 
+
+} 
+
+}
+
+}
